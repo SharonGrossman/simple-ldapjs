7ff49434ed79b7fa4d5437cc302b5ee48e8d5446
Sharon Grossman <sharong1337@gmail.com>
2016-12-16 17:03:58 +0200

    refactor(index): changed some things with promises and es6 default values

--- src/index.js
+++ src/index.js
@@ -2,46 +2,44 @@ import ldapjs from 'ldapjs';
 import pify from 'pify';
 import SimpleLdapError from './simple-ldap-error';
 
-export default pify(({principal = {}, url = process.env.LDAP_URL, user = process.env.LDAP_USER, password = process.env.LDAP_PASSWORD, ou = process.env.LDAP_OU}) => {
+export default pify(({principal = {}, url = process.env.LDAP_URL, user = process.env.LDAP_USER, password = process.env.LDAP_PASSWORD, ou = process.env.LDAP_OU} = {}) => {
   if (!url || !user || !password || !ou) {
     throw new SimpleLdapError('Simple LDAP failed at initializing the required parameters');
   }
 
   const client = ldapjs.createClient({url});
 
-  return new Promise(resolve => {
-    client.bind(user, password, err => {
+  return client.bind(user, password, err => {
+    if (err) {
+      throw new SimpleLdapError('Simple LDAP failed at binding to the user and password', err);
+    }
+
+    client.search(ou, {
+      filter: `userPrincipalName=${principal}`,
+      scope: 'sub'
+    }, (err, res) => {
       if (err) {
-        throw new SimpleLdapError('Simple LDAP failed at binding to the user and password', err);
+        throw new SimpleLdapError('Simple LDAP failed at searching and finding the user', err);
       }
 
-      client.search(ou, {
-        filter: `userPrincipalName=${principal}`,
-        scope: 'sub'
-      }, (err, res) => {
-        if (err) {
-          throw new SimpleLdapError('Simple LDAP failed at searching and finding the user', err);
-        }
+      let found;
 
-        let found;
+      res.once('searchEntry', entry => {
+        found = entry.object;
+        found._fullEntry = entry;
+      });
 
-        res.once('searchEntry', entry => {
-          found = entry.object;
-          found._fullEntry = entry;
-        });
+      res.on('end', res => {
+        client.unbind(err => {
+          if (err) {
+            throw new SimpleLdapError('Simple LDAP failed at unbinding the client', err);
+          }
 
-        res.on('end', res => {
-          client.unbind(err => {
-            if (err) {
-              throw new SimpleLdapError('Simple LDAP failed at unbinding the client', err);
-            }
+          if (res.status !== 0) {
+            throw new SimpleLdapError(`Simple LDAP failed with status code ${res.status}`);
+          }
 
-            if (res.status !== 0) {
-              throw new SimpleLdapError(`Simple LDAP failed with status code ${res.status}`);
-            }
-
-            return resolve(found);
-          });
+          return found;
         });
       });
     });
